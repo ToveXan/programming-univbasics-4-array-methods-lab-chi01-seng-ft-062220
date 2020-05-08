3c6733c2e21b165d738f135c96a26dda5c21d075
Zoe Xanos <zxanos@gmail.com>
2020-05-08 10:04:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,16 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+element1 = "wow"
+element2 = "Tardis"
+using_include(array, element2)
 
 def using_sort(array)
-
+  array.sort
 end
+sort_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+using_sort(sort_array)
 
 def using_reverse(array)
 
